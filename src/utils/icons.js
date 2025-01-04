8a02a59e4e71ba41e1e024305175cf9c4362017e
ritu-477 <ritujoharbishnoi@gmail.com>
2025-01-04 11:33:09 +0530

    chnages

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -1,5 +1,5 @@
-const Icon = ({ iconName, classStyle }) => {
-    const IconList = {
+const Icons = ({ iconName, classStyle }) => {
+    const IconsList = {
         doubleArrow: (
             <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="#fff" class="bi bi-chevron-double-up" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M7.646 2.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 3.707 2.354 9.354a.5.5 0 1 1-.708-.708z" />
@@ -11,9 +11,9 @@ const Icon = ({ iconName, classStyle }) => {
 
     return (
         <span className={`flex ${classStyle}`}>
-            {iconName && IconList[iconName]}
+            {iconName && IconsList[iconName]}
         </span>
     );
 };
 
-export default Icon;
+export default Icons;
